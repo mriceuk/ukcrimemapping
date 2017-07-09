710a4054cedd2a19bee928a06bc4a01dd4865105
Marc <marcrice83@googlemail.com>
2017-07-09 15:36:14 +0100

    refactoring panel

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,8 +3,63 @@ import { connect } from 'react-redux'
 import { getCrimes, updateFilters, updateGeocode } from '../actions'
 import Gmap from '../views/Gmap'
 
-class App extends Component {
 
+class LocationSelector extends Component {
+	
+  	render() {
+	  	
+	  	return (
+		<input id="options-panel--locationSelector" type="text" name="name" placeholder="Postcode or Place name" onChange={this.props.handleChange} />
+		)
+	}
+};
+
+class MonthSelector extends Component {
+	
+	render() {
+		
+		let currentYear = new Date().getFullYear();
+		let years = [];
+		let startYear = 1980;
+
+        while ( currentYear > startYear ) {
+                    years.push(startYear--);
+        } 
+        console.log(years);
+		
+		return (
+			<div className='month-selector'>
+				<select id='options-panel--year' name='year' onChange={this.props.handleChange} >
+			      	<option selected="selected">2017</option>
+			      	<option >2016</option>
+			      	<option>2015</option>
+			      	<option>2014</option>
+			      	<option>2013</option>
+			      	<option>2012</option>
+			      </select>
+			      
+			      <select id='options-panel--month' name='month' onChange={this.props.handleChange} >
+			      	<option>12</option>
+			      	<option>11</option>
+			      	<option>10</option>
+			      	<option>9</option>
+			      	<option>8</option>
+			      	<option>7</option>
+			      	<option>6</option>	
+			      	<option >5</option>	
+			      	<option selected="selected">4</option>		
+			      	<option>3</option>		
+			      	<option>2</option>		
+			      	<option>1</option>			      		      		      		      			      		      			      	
+				</select>
+			</div>
+		)
+		
+	}
+	
+};
+
+class App extends Component {
 
   componentDidMount() {
     const { dispatch, filters } = this.props
@@ -19,6 +74,7 @@ class App extends Component {
   }
 
   handleChange = event => {
+ 
     this.props.dispatch( updateFilters({
 	    location: document.getElementById('options-panel--locationSelector').value,
 	    year: document.getElementById('options-panel--year').value,
@@ -38,40 +94,24 @@ class App extends Component {
   
   render() {
 
-  	let initialCoordinates = { lat: 51.5073509, lng:  -0.1277583 }
-  	
     return (
-      <div>
-	  		<div className='options-panel'>
-		      <input id="options-panel--locationSelector" type="text" name="name" placeholder="Postcode or Place name" onChange={this.handleChange} />
-		      <select id='options-panel--year' name='year' onChange={this.handleChange}>
-		      	<option selected="selected">2017</option>
-		      	<option >2016</option>
-		      	<option>2015</option>
-		      	<option>2014</option>
-		      	<option>2013</option>
-		      	<option>2012</option>
-		      	
-		      </select>
-		      <select id='options-panel--month' name='month' onChange={this.handleChange}>
-		      	<option>12</option>
-		      	<option>11</option>
-		      	<option>10</option>
-		      	<option>9</option>
-		      	<option>8</option>
-		      	<option>7</option>
-		      	<option>6</option>	
-		      	<option >5</option>	
-		      	<option selected="selected">4</option>		
-		      	<option>3</option>		
-		      	<option>2</option>		
-		      	<option>1</option>			      		      		      		      			      		      			      	
-		      </select>
-		      <input type="button" name="button" value="submit" className="chunky-button" onClick={this.handleSubmit}/>
-		      <div className='status'>{this.props.filters.status}</div>
+	   
+      <form onchange>
+      		
+	  	<div className='options-panel'>
+	  		
+  			<LocationSelector handleChange={this.handleChange}/>
+  			<MonthSelector handleChange={this.handleChange}/>
+			<input type="button" name="button" value="submit" onClick={this.handleSubmit}/>
+			<div className='status'>{this.props.filters.status}</div>
+				
 	    </div>
-			<div id='gmapwrap'><Gmap center={ this.props.filters.geocode }  crimes={this.props.filters.crimes} /></div>
-      </div>
+	    
+		<div id='gmapwrap'>
+			<Gmap center={ this.props.filters.geocode }  crimes={this.props.filters.crimes} />
+		</div>
+		
+      </form>
     )
     
   }
